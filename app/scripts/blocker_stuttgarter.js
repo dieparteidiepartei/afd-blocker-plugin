133325486c02c9761c309e7402c4f5d799df8844
CWollinger <CWollinger@web.de>
2019-05-13 19:06:49 +0200

    Stuttgarter Zeitung

---
+++ app/scripts/blocker_stuttgarter.js
@@ -0,0 +1,23 @@
+console.log("#### AfD CONTENT-BLOCKER ####");
+
+import { Blocker } from "./blocker_general";
+
+let blocker = new Blocker([
+
+    {
+        selector: 'article',
+        type: 'big'
+    },
+    {
+        selector: 'article mod-teaser',
+        type: 'big'
+    },
+    {
+        selector: '.box-head',
+        type: 'big'
+    }
+]);
+
+blocker.modifyContent([document]);
+blocker.watchPageForMutations();
+
