cb5ab707a20283ea797196334ebb027e629fb26a
DIE PARTEI <ep2019@die-partei.de>
2019-05-14 21:46:10 +0200

    additional selectors for twitter

--- app/scripts/blocker_twitter.js
+++ app/scripts/blocker_twitter.js
@@ -3,7 +3,14 @@ console.log("#### AfD CONTENT-BLOCKER ####");
 import { Blocker } from "./blocker_general";
 
 let blocker = new Blocker([
-
+    {
+        selector: 'div.tweet',
+        type: 'big'
+    },
+    {
+        selector: 'div.ProfileCard',
+        type: 'big'
+    },
     {
         selector: 'article',
         type: 'big'
@@ -12,8 +19,6 @@ let blocker = new Blocker([
         selector: 'div.entrylist__entry',
         type: 'small'
     },
-
-
 ]);
 
 blocker.modifyContent([document]);
