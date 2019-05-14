b5f9f6986f0bf136ce6bda2701a922488ed60758
DIE PARTEI <ep2019@die-partei.de>
2019-05-14 21:42:06 +0200

    bigger blocker sizes for facebook

--- app/scripts/blocker_facebook.js
+++ app/scripts/blocker_facebook.js
@@ -4,6 +4,18 @@ import { Blocker } from "./blocker_general";
 
 let blocker = new Blocker([
     {
+        selector: '._77we',
+        type: 'big'
+    },
+    {
+        selector: '._6-e5',
+        type: 'big'
+    },
+    {
+        selector: '._4b0l',
+        type: 'big'
+    },
+    {
         selector: '._4b0l',
         type: 'big'
     },
