096b7225eb2140e4fc4fc7245df1251e66b82ba4
Florian Drechsler <f.drechsler@headtrip.eu>
2019-05-15 10:19:46 +0200

    added nordbayern.de

---
+++ app/scripts/blocker_nordbayern.js
@@ -0,0 +1,51 @@
+/**
+ * Author: Florian Drechsler, @fdrechsler
+ *
+ * nordbayern.de
+ *
+ * Online Plattform von:
+ * - Nürnberger Nachrichten
+ * - NZ Nürnberger Zeitung
+ * - Erlanger Nachrichten
+ * - Fürther Nachrichten
+ * - Neumarkter nachrichten
+ * ....
+ */
+
+console.log("#### AfD CONTENT-BLOCKER ####");
+
+import { Blocker } from "./blocker_general";
+
+let blocker = new Blocker([
+    {
+        selector: '.search-item',
+        type: 'small'
+    },
+    {
+        selector: '.topmodulXXL',
+        type: 'big'
+    },
+    {
+        selector: '.xxxlItem1',
+        type: 'small'
+    },
+    {
+        selector: '.xxxlItem2',
+        type: 'small'
+    },
+    {
+        selector: '.xxxlItem3',
+        type: 'small'
+    },
+    {
+        selector: '.mediabox',
+        type: 'small'
+    },
+    {
+        selector: '.article-content',
+        type: 'big'
+    }
+]);
+
+blocker.modifyContent([document]);
+blocker.watchPageForMutations();
